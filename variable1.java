e495728c1a2ca64cb84b2605351518380d7ba62f
roshan32366 <roshan29225@gmail.com>
2025-06-16 10:06:35 +0530

    Add files via upload

---
+++ variable1.java
@@ -0,0 +1,19 @@
+public class variable1 {
+    public static void main(String[] args) {
+        int myNumber = 867;
+        int yourNumber = 86;
+        System.out.println(yourNumber);
+        System.out.println(yourNumber);
+        System.out.println(myNumber);
+        boolean isVegetratian = true;
+        System.out.println(isVegetratian);
+        String wishes = "Good Morning";
+        System.out.println(wishes);
+        int myRoshan=344;
+        int your_sachin_name=688;
+        int  tree =899;
+        System.out.println(tree);
+
+
+    }
+}
